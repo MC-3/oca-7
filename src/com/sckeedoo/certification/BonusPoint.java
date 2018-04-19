7cf4b84684a3109919015ab622a667c94c12e558
MC-3 <37633639+MC-3@users.noreply.github.com>
2018-04-19 21:33:31 +0300

    Add files via upload

--- src/com/sckeedoo/certification/BonusPoint.java
+++ src/com/sckeedoo/certification/BonusPoint.java
@@ -1,9 +1,8 @@
-package com.sckeedoo.certification;
 
+package com.sckeedo.certification;
+import java.util.Scanner;
 public class BonusPoint {
-
-    /**
-     *
+    /*
      * You probably have used the trolleybus in Chisinau.
      * Every time you enter in trolleybus you have to get a ticked from the taxator otherwise you will be a iepurash
      * Every ticket has a number of it.
@@ -18,7 +17,55 @@ public class BonusPoint {
      *    123999        simple ticket
      *    123321        super lucky
      */
+    
     public static void main(String[] args) {
-
+      boolean ver; 
+      final int a= 6;
+      int var= 2;
+      int str[] = new int [a];
+      do
+        {   ver = true;
+            System.out.println("Enter ticket number: ");
+            Scanner in = new Scanner(System.in);
+            String str1 = in.next();
+             if (str1.length()!=a)
+            {  System.out.println("Incorrect number's length"); ver = false;  }
+            else
+            {  
+                for(int i = 0; i < a; i++)
+                    {
+                        str[i] = str1.charAt(i)-48;
+                        if ((str[i]<0)||(str[i]>9))
+                        {  System.out.println("Entered incorrect number");
+                           ver = false; break;}
+                    }
+            }
+        } while (!ver);
+      
+      for (int i=0; i<a/2; ++i)
+        { 
+          if ((str[i]==str[a-1-i]) && (var==2)){}
+          else { var=1; break; }
+        }
+      if (var==2){ System.out.println("Super lucky ticket, palindrome!!!"); }
+      else 
+      {
+        int sum1=0, sum2=0;
+        for (int i=0; i<a/2; ++i)
+        { 
+           sum1 += str[i];
+           sum2 += str[a-1-i]; 
+        }
+        if (sum1==sum2){ System.out.println("Lucky ticket"); }
+        else { var=0; System.out.println("Simple ticket"); }               
+      }
+      /*
+      switch (var)
+      {
+          case 0: { System.out.println("Simple ticket"); } break;
+          case 1: { System.out.println("Lucky ticket"); } break;
+          case 2: { System.out.println("Super lucky ticket"); } break;
+      }*/
     }
 }
+    
